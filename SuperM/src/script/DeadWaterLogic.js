479ca6263c21597f84d1c491af49263a2f890689
sw28363807 <sw849557369@gmail.com>
2020-08-30 23:45:31 +0800

    no message

--- SuperM/src/script/DeadWaterLogic.js
+++ SuperM/src/script/DeadWaterLogic.js
@@ -6,6 +6,10 @@ export default class DeadWaterLogic extends Laya.Script {
         super();
         /** @prop {name:resetPosX, tips:"重置位置", type:Int, default:0}*/
         let resetPosX = 0;
+        /** @prop {name:upHeight, tips:"上浮位置", type:Int, default:0}*/
+        let upHeight = 0;
+        /** @prop {name:upTime, tips:"上浮持续时间", type:Int, default:0}*/
+        let upTime = 0;
     }
     
     onEnable() {
@@ -20,6 +24,18 @@ export default class DeadWaterLogic extends Laya.Script {
             this.owner.resetPosX = script.resetPosX;
         } else {
             this.owner.resetPosX = 0;
+        }
+
+        if (script.upHeight) {
+            this.owner.upHeight = script.upHeight;
+        } else {
+            this.owner.upHeight = 0;
+        }
+
+        if (script.upTime) {
+            this.owner.upTime = script.upTime;
+        } else {
+            this.owner.upTime = 0;
         }
     }
 
