f3acd505070e5930371abba907e7715c484a158f
sw28363807 <sw849557369@gmail.com>
2020-09-19 10:54:29 +0800

    no message

--- SuperM/src/script/BlackBoxLogic.js
+++ SuperM/src/script/BlackBoxLogic.js
@@ -9,8 +9,10 @@ export default class BlackBoxLogic extends Laya.Script {
     onEnable() {
         this.owner.goldLabel = this.owner.getChildByName("goldLabel");
         this.owner.roleLabel = this.owner.getChildByName("roleLabel");
+        this.owner.fenLabel = this.owner.getChildByName("fenLabel");
         this.owner.goldLabel.text = "x"+String( GameContext.gameGoldNumber);
         this.owner.roleLabel.text = "x"+String(GameContext.gameRoleNumber);
+        this.owner.fenLabel.text = "x"+String(GameContext.roleFen);
     }
 
     onDisable() {
