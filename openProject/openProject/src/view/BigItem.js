5178b971007b954d1409dcedfbdefa2b5b38b293
sw28363807 <sw849557369@gmail.com>
2020-09-23 19:26:21 +0800

    no message

--- openProject/openProject/src/view/BigItem.js
+++ openProject/openProject/src/view/BigItem.js
@@ -8,9 +8,33 @@ export default class BigItem extends BigItemUI {
     set dataSource(value){
         if(!value)
             return;
-            this.text_rank.text = String(value.rankIndex + 1);
+            let rankNumber = value.rankIndex + 1;
+            this.text_rank.text = String(rankNumber);
             this.img_head.skin = value.avatarIP;
             this.text_name.text = value.UserName;
             this.text_score.text = value.RankValue;
+
+
+            if (rankNumber == 1) {
+                this.text_rank.text.visible = false;
+                this.rank1.visible = true;
+                this.rank2.visible = false;
+                this.rank3.visible = false;
+            } else if (rankNumber == 2) {
+                this.text_rank.text.visible = false;
+                this.rank1.visible = false;
+                this.rank2.visible = true;
+                this.rank3.visible = false;
+            } else if (rankNumber == 3) {
+                this.text_rank.text.visible = false;
+                this.rank1.visible = false;
+                this.rank2.visible = false;
+                this.rank3.visible = true;
+            } else {
+                this.text_rank.text.visible = true;
+                this.rank1.visible = false;
+                this.rank2.visible = false;
+                this.rank3.visible = false;
+            }
     }
 }
